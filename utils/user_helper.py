3a88d8ee0d361f6e349b6106cec43220309b2a3c
user <ahmadboyabdurahimov589@gmail.com>
2024-07-08 10:41:00 +0500

    new changes

--- utils/user_helper.py
+++ utils/user_helper.py
@@ -1,12 +1,18 @@
+import typing
+
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from utils import jwt
-from app.models import user as user_models
 from app.api.deps import get_db
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from typing import Union
 from jwt.exceptions import InvalidTokenError, DecodeError, ExpiredSignatureError
+
+
+if typing.TYPE_CHECKING:
+    from app.models import user as user_models
+
 
 http_bearer = HTTPBearer()
 
@@ -37,7 +43,7 @@ async def get_current_token_payload(
 async def get_current_user(
         payload: dict = Depends(get_current_token_payload),
         db: AsyncSession = Depends(get_db)
-) -> Union[user_models.User, None]:
+) -> Union['user_models.User', None]:
 
     result = await db.execute(select(user_models.User).filter(
         user_models.User.id == payload['sub'], user_models.User.username == payload['username']
