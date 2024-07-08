2ae4fc1c868f19f2282161fb93c62113c9180b2e
user <ahmadboyabdurahimov589@gmail.com>
2024-07-08 23:41:37 +0500

    new changes

--- app/api/views/auth.py
+++ app/api/views/auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.schemas import auth
