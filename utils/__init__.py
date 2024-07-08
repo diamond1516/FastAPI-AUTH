dbb7db97769225ca1420fd4058f3801cf10995a0
user <ahmadboyabdurahimov589@gmail.com>
2024-07-08 10:33:45 +0500

    new changes

---
+++ utils/__init__.py
@@ -0,0 +1,44 @@
+__all__ = (
+    'BasePermission',
+    'get_current_user',
+    'hash_password',
+    'verify_password',
+    'encode_jwt',
+    'decode_jwt',
+    'UTILITY',
+)
+
+
+from fastapi import Depends, Request, HTTPException, status
+from utils.user_helper import get_current_user
+from utils.password import hash_password, verify_password
+from utils.utility import UTILITY
+from utils.jwt import encode_jwt, decode_jwt
+
+
+class BasePermission:
+
+    """
+    This is the base permission class that all permissions must inherit from.
+    """
+
+    async def has_permission(self, user, request, view=None):
+        """
+        >>> self.has_permission(user, request, view)
+        """
+
+        raise NotImplementedError
+
+
+async def check_permission(permission: BasePermission):
+    async def permission_checker(
+            user=Depends(get_current_user),
+            request: Request = Depends(),
+    ):
+
+        if not permission.has_permission(user, request):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN, detail="You do not have permission to perform this action"
+            )
+
+    return permission_checker
