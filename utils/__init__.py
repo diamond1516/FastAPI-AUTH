0dc48478ce264dcbb2103ffed59161f9fbc32fe7
user <ahmadboyabdurahimov589@gmail.com>
2024-07-08 11:14:39 +0500

    new changes

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 __all__ = (
     'BasePermission',
+    'get_user_with_permissions',
     'get_current_user',
     'hash_password',
     'verify_password',
@@ -8,16 +9,18 @@ __all__ = (
     'UTILITY',
 )
 
-
 from fastapi import Depends, Request, HTTPException, status
 from utils.user_helper import get_current_user
 from utils.password import hash_password, verify_password
 from utils.utility import UTILITY
 from utils.jwt import encode_jwt, decode_jwt
+from typing import Union, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.models.user import User
 
 
 class BasePermission:
-
     """
     This is the base permission class that all permissions must inherit from.
     """
@@ -30,15 +33,17 @@ class BasePermission:
         raise NotImplementedError
 
 
-async def check_permission(permission: BasePermission):
+async def get_user_with_permissions(*permissions: BasePermission):
     async def permission_checker(
             user=Depends(get_current_user),
             request: Request = Depends(),
-    ):
+    ) -> Union['User', None]:
 
-        if not permission.has_permission(user, request):
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN, detail="You do not have permission to perform this action"
-            )
+        for permission in permissions:
+            if not await permission.has_permission(user, request):
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN, detail="You do not have permission to perform this action"
+                )
+        return user  # return user
 
     return permission_checker
