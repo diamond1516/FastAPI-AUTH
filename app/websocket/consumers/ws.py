056968e2564c4713e7b0885b3e9bff0163bf8111
user <ahmadboyabdurahimov589@gmail.com>
2024-07-09 12:25:36 +0500

    new changes

--- app/websocket/consumers/ws.py
+++ app/websocket/consumers/ws.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from app.core import socket_manager
 
@@ -12,8 +14,14 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await socket_manager.connect(websocket)
     try:
         while True:
-            data = await websocket.receive_text()
-            await socket_manager.broadcast(f"Client #{client_id} says: {data}")
+            receive_task = asyncio.create_task(websocket.receive_text())
+            done, pending = await asyncio.wait(
+                [receive_task],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+            if receive_task in done:
+                data = receive_task.result()
+                await socket_manager.broadcast(f"Client #{client_id} says: {data}")
     except WebSocketDisconnect:
         socket_manager.disconnect(websocket)
         await socket_manager.broadcast(f"Client #{client_id} disconnected")
