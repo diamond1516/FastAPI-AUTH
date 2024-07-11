0937d634a389645057522b1db2936a6b1a141344
user <ahmadboyabdurahimov589@gmail.com>
2024-07-11 15:36:09 +0500

    new changes

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -5,8 +5,3 @@ __all__ = (
 from app.db.base import Base
 from app.db.database import db_helper
 
-
-#
-# async def creat_all():
-#     async with db_helper.engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
